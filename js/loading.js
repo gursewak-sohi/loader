44c17bd908c749c7676f73cd827d488bd4ec7693
liverpool09 <sohi.gursewak@gmail.com>
2022-04-20 13:32:00 +0530

    smooth animation

--- js/loading.js
+++ js/loading.js
@@ -10,9 +10,10 @@
         slideThree = document.querySelector('[data-slide="3"]'),
         slideFour = document.querySelector('[data-slide="4"]'),
         slideFive = document.querySelector('[data-slide="5"]'),
-        progress = document.querySelector(".progress");
+        progress = document.querySelector(".progress"),
+        progressWrapper = document.querySelector(".progress-bar");
 
-    if (modalLink && modalID && slideOne && slideTwo && slideThree && slideFour && slideFive && progress) {
+    if (modalLink && modalID && slideOne && slideThree && slideFour && slideFive && progress) {
         modalLink.onclick = (el) => {
                 let id = el.currentTarget.dataset.modal;
                 document.querySelector("#" + id).classList.add("active");
@@ -25,62 +26,61 @@
             console.time();
             slideOne.classList.add('active');
             slideFive.classList.remove('active');
-            progress.classList.remove('progress-red');
-            progress.classList.add('progress-green');
+            // progress.classList.remove('progress-red');
+            // progress.classList.add('progress-green');
             setTimeout(() => {
                 slideOne.classList.remove('active');
-                slideTwo.classList.add('active');
+                slideThree.classList.add('active');
+                // setTimeout(() => {
+                //     slideTwo.classList.remove('active');
+                //     slideThree.classList.add('active');
+                // Call Progress (Will take total 5 seconds, 3s animation + 2s wait time at 95 progress)
+                progress.classList.add('progress-start');
+                progressBar();
                 setTimeout(() => {
-                    slideTwo.classList.remove('active');
-                    slideThree.classList.add('active');
-                    // Call Progress (Will take total 5 seconds, 3s animation + 2s wait time at 95 progress)
-                    progressBar();
+                    // alert("Animation bar stopped");
+                    slideThree.classList.remove('active');
+                    slideFour.classList.add('active');
                     setTimeout(() => {
-                        // alert("Animation bar stopped");
-                        slideThree.classList.remove('active');
-                        slideFour.classList.add('active');
+                        slideFour.classList.remove('active');
+                        slideFive.classList.add('active');
                         setTimeout(() => {
-                            slideFour.classList.remove('active');
-                            slideFive.classList.add('active');
-                            setTimeout(() => {
-                                // End Time
-                                console.timeEnd();
-                                slideFive.classList.remove('active');
-                                modalID.classList.remove('active');
-                            }, 2000);
+                            // End Time
+                            console.timeEnd();
+                            slideFive.classList.remove('active');
+                            modalID.classList.remove('active');
                         }, 2000);
-                    }, 5300);
-                }, 1000);
+                    }, 2000);
+                }, 7000);
+                // }, 1000);
             }, 3000);
 
             // Progress Bar Function
             const progressBar = () => {
                 let i = 0;
+                let width = 1;
                 if (i == 0) {
                     i = 1;
-                    let width = 1;
                     const frame = () => {
-                        if (width >= 100) {
+                        if (width >= 95) {
                             clearInterval(interval);
+
+                            progressWrapper.classList.add('blink');
+                            setTimeout(() => {
+                                progressWrapper.classList.remove('blink');
+                            }, 3000);
                             i = 0;
                         } else {
                             if (width >= 60) {
-                                progress.classList.remove('progress-green');
-                                progress.classList.add('progress-red');
-                            }
-                            if (width == 94) {
-                                clearInterval(interval);
-                                progress.classList.add('blink');
-                                setTimeout(() => {
-                                    progress.classList.remove('blink');
-                                }, 2000);
+                                // progress.classList.remove('progress-green');
+                                // progress.classList.add('progress-red');
                             }
                             width++;
-                            progress.style.width = width + "%";
+                            // progress.style.width = width + "%";
                             progress.innerHTML = width + "%";
                         }
                     }
-                    let interval = setInterval(frame, 3 * 1000 / 100);
+                    let interval = setInterval(frame, 5 * 1000 / 100);
                 }
             }
         }
