fc702fd9ae0cd066f03b256ba801e81d70aef69d
liverpool09 <sohi.gursewak@gmail.com>
2022-04-21 17:33:43 +0530

    code update

--- js/loading.js
+++ js/loading.js
@@ -1,26 +1,26 @@
-// Loading JS
-
 (function() {
     // Show Modals 
 
-    let modalLink = document.querySelector('[data-modal]'),
-        modalID = document.querySelector('#loaderModal'),
+    // In Minutes (0.5 = 30 seconds)
+    // let animStartTime = 4.33;
+    let animStartTime = 0.1;
+    let modalID = document.querySelector('#loaderModal'),
         slideOne = document.querySelector('[data-slide="1"]'),
         slideTwo = document.querySelector('[data-slide="2"]'),
         slideThree = document.querySelector('[data-slide="3"]'),
         slideFour = document.querySelector('[data-slide="4"]'),
         slideFive = document.querySelector('[data-slide="5"]'),
-        progress = document.querySelector(".progress"),
-        progressWrapper = document.querySelector(".progress-bar");
+        slideSix = document.querySelector('[data-slide="6"]'),
+        progress = document.querySelector(".c-progress"),
+        progressWrapper = document.querySelector(".c-progress-bar");
 
     if (modalID) {
-        modalLink.onclick = (el) => {
-                let id = el.currentTarget.dataset.modal;
-                document.querySelector("#" + id).classList.add("active");
-                // Call Animations
-                animations();
-            }
-            // Animations Function
+        setTimeout(() => {
+            modalID.classList.add("active");
+            animations();
+        }, animStartTime * 60 * 1000);
+
+        // Animations Function
         const animations = () => {
             // Start Time
             console.time();
@@ -32,7 +32,7 @@
                 setTimeout(() => {
                     slideTwo.classList.remove('active');
                     slideThree.classList.add('active');
-                    // Call Progress (Will take total 5 seconds, 3s animation + 2s wait time at 95 progress)
+                    // Call Progress (Will take total 7 seconds, 5s animation + 2s wait time at 95 progress)
                     progress.classList.add('progress-start');
                     progressBar();
                     setTimeout(() => {
@@ -43,12 +43,16 @@
                             slideFour.classList.remove('active');
                             slideFive.classList.add('active');
                             setTimeout(() => {
-                                // End Time
-                                console.timeEnd();
                                 slideFive.classList.remove('active');
-                                modalID.classList.remove('active');
-                            }, 2000);
-                        }, 2000);
+                                slideSix.classList.add('active');
+                                setTimeout(() => {
+                                    // End Time
+                                    console.timeEnd();
+                                    slideSix.classList.remove('active');
+                                    modalID.classList.remove('active');
+                                }, 3000);
+                            }, 4000);
+                        }, 4000);
                     }, 7000);
                 }, 2000);
             }, 3000);
